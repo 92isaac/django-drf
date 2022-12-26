7f5cb4089a558f43834b5574b41525553b414969
Sangodare Isaac <sangodareisaac92@gmail.com>
2022-12-26 16:51:18 +0100

    add crud operation to views.py

--- baseproject/views.py
+++ baseproject/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Developer
 from .serializers import DeveloperSerializer
+from django.db.models import Q
 
 # Create your views here.
 # def default_endpoint(request):
@@ -24,19 +25,54 @@ def default_endpoint(request):
     data = ['developers', '/developer/:details']
     return Response(data)
     
-@api_view(['GET'])
+@api_view(['GET', 'POST', 'PUT'])
 def developers(request):
     # data = ['tayo', 'isaac', 'tumininu', 'lawyer kunle', 'rowland']
-    # using object,all or object.filter to get all and query as well
     # developers = Developer.objects.all()
 
-    developers = Developer.objects.filter()
-    serializer = DeveloperSerializer(developers, many=True)
-    return Response(serializer.data)
+    # using object.all over object.filter to get all and query as well e.g http://127.0.0.1:5050/developers/?query=react
 
-@api_view(['GET'])
+    # conditional statements to check what leb\vel of restdullness the api is 
+    # if request is get request
+    if request.method == 'GET':
+        query = request.GET.get('query')
+        if query is None:
+            query = ''
+
+        developers = Developer.objects.filter(Q(username__icontains=query) | Q(bio__icontains=query))
+        serializer = DeveloperSerializer(developers, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        create_developer = Developer.objects.create(username=request.data['username'], bio=request.data['bio'])
+        serializer =DeveloperSerializer(create_developer, many=False)
+        return Response(serializer.data)
+        # return Response('developer added successfully')
+
+
+    
+# @api_view(['POST'])
+# def add_developer(request):
+#     Developer.objects.create(username=request.data['username'])
+#     return Response('developer added successfully')
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def developer_detail(request, username):
     # data = username
     developer = Developer.objects.get(username=username)
-    serializer = DeveloperSerializer(developer)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        # developer = Developer.objects.get(username=username)
+        serializer = DeveloperSerializer(developer)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        developer.username = request.data['username']
+        developer.bio = request.data['bio']
+        developer.save()
+        serializer = DeveloperSerializer(developer)
+        return Response(serializer.data)
+
+    if request.method == 'DELETE':
+        developer.delete()
+        return Response("Developer deleted")
+
+# its not a must every api is restful
+# rest api, fast api,  soap api type of apis are just structures of building api
